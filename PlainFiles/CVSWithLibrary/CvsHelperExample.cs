f7cbaf5e3da4d30e068150a2bcfcd617b44eb0b9
mvidalesg <miltonvidales77758@correo.itm.edu.co>
2025-06-02 17:18:23 -0500

    Cvs With Library

---
+++ PlainFiles/CVSWithLibrary/CvsHelperExample.cs
@@ -0,0 +1,26 @@
+﻿using CsvHelper;
+using System.Globalization;
+using System.Windows.Markup;
+
+namespace CVSWithLibrary;
+
+    internal class CvsHelperExample
+{
+    public void write(string path, IEnumerable<Person> people)
+    {
+        using var sw = new StreamWriter(path);
+        using var cw = new CsvWriter(sw, CultureInfo.InvariantCulture);
+        cw.WriteRecords(people);
+    }
+    public IEnumerable<Person> read(string path)
+    {
+        if (!File.Exists(path))
+        {
+            return Enumerable.Empty<Person>();
+        }
+        using var sr = new StreamReader(path);
+        using var cr = new CsvReader(sr, CultureInfo.InvariantCulture);
+        return cr.GetRecords<Person>().ToList();
+    }
+}
+
---
+++ PlainFiles/CVSWithLibrary/Program.cs
@@ -0,0 +1,16 @@
+﻿using CVSWithLibrary;
+
+var list = new List<Person>
+{
+    new Person { Id = 1, Name = "Maria", Age = 28 },
+    new Person { Id = 2, Name = "tomas", Age = 24 },
+    
+};  
+var helper = new CvsHelperExample();
+helper.write("people.csv", list);
+
+var readPeople = helper.read("people.csv");
+foreach (var person in readPeople)
+{
+    Console.WriteLine($"Id: {person.Id}, Name: {person.Name}, Age: {person.Age}");
+}
